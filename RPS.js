66e172f0c69cfc9ff66458f82da8660cef501ece
Mr-Megiddo <johnmegiddo@gmail.com>
2025-02-07 15:25:03 +0530

    Added local storage for updating score

--- RPS.js
+++ RPS.js
@@ -1,53 +1,58 @@
-let wins = 0;
-let losses = 0;
-let ties = 0;
+  const score = JSON.parse(localStorage.getItem("saveScore")) || {
+    wins : 0,
+    losses : 0,
+    ties : 0
+  };
 
-function playGame(a){
-  let computerChoice;
-  let myMove;
-  let computerMove;
-  let result;
+  function playGame(a){
+    let computerChoice;
+    let myMove;
+    let computerMove;
+    let result;
 
-  myMove = a.toLowerCase();
-  computerChoice = Math.floor(Math.random()*3);
+    myMove = a.toLowerCase();
+    computerChoice = Math.floor(Math.random()*3);
 
-  computerChoice === 0 ? computerMove="Rock"
-  : computerChoice === 1 ? computerMove = "Paper"
-  : computerMove = "Scissors"
+    computerChoice === 0 ? computerMove="Rock"
+    : computerChoice === 1 ? computerMove = "Paper"
+    : computerMove = "Scissors"
 
-  if(myMove === "scissors"){
-    computerMove === "Scissors" ? result="Tied"
-    : computerMove === "Rock" ? result="You lost"
-    : result = "You win"
+    if(myMove === "scissors"){
+      computerMove === "Scissors" ? result="Tied"
+      : computerMove === "Rock" ? result="You lost"
+      : result = "You win"
+    }
+    else if(myMove === "rock"){
+      computerMove === "Rock" ? result="Tied"
+      :computerMove === "Paper" ? result="You Lost"
+      : result = "You win"
+    }
+    else if(myMove === "paper"){
+      computerMove === "Paper" ? result="Tied"
+      :computerMove === "Scissors" ? result="You Lost"
+      : result = "You win"
+    }
+
+    const Declare = document.getElementById("result");
+    Declare.innerHTML = `<h3>${result}</h3> 
+    <h4>You pick ${myMove}</h4> 
+    <h4>Computer pick ${computerMove}</h4>`;
+
+    if(result === "You win"){
+      score.wins = score.wins += 1;
+    }else if(result === "You Lost"){
+      score.losses = score.losses += 1;
+    }else{
+      score.ties = score.ties += 1;
+    }
+
+    const footer = document.getElementById("footer");
+    footer.innerHTML = `<p>Wins : ${score.wins} </p> <p>Losses : ${score.losses} </p> <p>Ties : ${score.ties} </p>`;  
+
+    localStorage.setItem("saveScore", JSON.stringify(score));
   }
-  else if(myMove === "rock"){
-    computerMove === "Rock" ? result="Tied"
-    :computerMove === "Paper" ? result="You Lost"
-    : result = "You win"
+
+  function resetScore(){
+    localStorage.removeItem("saveScore");
+    location.reload();
   }
-  else if(myMove === "paper"){
-    computerMove === "Paper" ? result="Tied"
-    :computerMove === "Scissors" ? result="You Lost"
-    : result = "You win"
-  }
-
-  const Declare = document.getElementById("result");
-  Declare.innerHTML = `<h3>${result}</h3> 
-  <h4>You pick ${myMove}</h4> 
-  <h4>Computer pick ${computerMove}</h4>`;
-
-  if(result === "You win"){
-    wins = wins+=1;
-  }else if(result === "You Lost"){
-    losses = losses+=1;
-  }else{
-    ties = ties+=1;
-  }
-
-  const footer = document.getElementById("footer");
-  footer.innerHTML = `<p>Wins : ${wins} </p> <p>Losses : ${losses} </p> <p>Ties : ${ties} </p>`;  
-}
-
-function resetScore(){
-  location.reload();
-}
